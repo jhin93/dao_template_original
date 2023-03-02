6d792ef4e504df9098b4933a72fb85d831e9a29c
jhin93 <scene1993@gmail.com>
2023-03-02 21:47:08 +0900

    node module install

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
   uint256 public s_maxSupply = 1000000000000000000000000;
